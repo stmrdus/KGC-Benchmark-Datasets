4a153eae992e41d241120ddab845ccd0f4646d4a
nhutnamhcmus <sakura9x016@gmail.com>
2022-01-29 17:35:21 +0700

    update data

--- n-n.py
+++ n-n.py
@@ -3,7 +3,7 @@ import numpy as np
 from argparse import ArgumentParser
 
 parser = ArgumentParser("Relation types script")
-parser.add_argument("--folder", default="WN18RR", help="Name of dataset folder.")
+parser.add_argument("--folder", default="FB15K-237", help="Name of dataset folder.")
 args = parser.parse_args()
 print(args)
 
@@ -33,7 +33,7 @@ def add_rel_category(rel_category, new_data):
     else:
         return pd.DataFrame(data=np.array([new_data.to_numpy()]))
 
-for index, row in test_data.iterrows():
+for index, row in train_data.iterrows():
     rel = row[1]
     hptr_temp = hptr.loc[rel]
     tphr_temp = tphr.loc[rel]
@@ -47,12 +47,51 @@ for index, row in test_data.iterrows():
         test_datan_n = add_rel_category(test_datan_n, row)
 
 
-test_data1_1.to_csv((args.folder + "/test/1-1.txt"), sep='\t', header=None, index=None)
-test_data1_n.to_csv((args.folder + "/test/1-n.txt"), sep='\t', header=None, index=None)
-test_datan_1.to_csv((args.folder + "/test/n-1.txt"), sep='\t', header=None, index=None)
-test_datan_n.to_csv((args.folder + "/test/n-n.txt"), sep='\t', header=None, index=None)
+test_data1_1.to_csv((args.folder + "/train/1-1.txt"), sep='\t', header=None, index=None)
+test_data1_n.to_csv((args.folder + "/train/1-n.txt"), sep='\t', header=None, index=None)
+test_datan_1.to_csv((args.folder + "/train/n-1.txt"), sep='\t', header=None, index=None)
+test_datan_n.to_csv((args.folder + "/train/n-n.txt"), sep='\t', header=None, index=None)
 
 test_data_grouped = test_data.groupby(1)
 
+count = 0
 for k, gr in test_data_grouped:
-    gr.to_csv((args.folder + "/test/{}.txt".format(k)), sep='\t', encoding='utf-8', index=False, header=False, mode='a')
+    name = "relation_" + str(count)
+    gr.to_csv((args.folder + "/train/{}.txt".format(name)), sep='\t', encoding='utf-8', index=False, header=False, mode='a')
+    count += 1
+
+
+test_data1_1, test_data1_n, test_datan_1, test_datan_n = None, None, None, None
+
+def add_rel_category(rel_category, new_data):
+    if rel_category is not None:
+        return rel_category.append(new_data)
+    else:
+        return pd.DataFrame(data=np.array([new_data.to_numpy()]))
+
+for index, row in valid_data.iterrows():
+    rel = row[1]
+    hptr_temp = hptr.loc[rel]
+    tphr_temp = tphr.loc[rel]
+    if hptr_temp < 1.5 and tphr_temp < 1.5:
+        test_data1_1 = add_rel_category(test_data1_1, row)
+    elif hptr_temp < 1.5 and tphr_temp >= 1.5:
+        test_data1_n = add_rel_category(test_data1_n, row)
+    elif hptr_temp >= 1.5 and tphr_temp < 1.5:
+        test_datan_1 = add_rel_category(test_datan_1, row)
+    elif hptr_temp >= 1.5 and tphr_temp >= 1.5:
+        test_datan_n = add_rel_category(test_datan_n, row)
+
+
+#test_data1_1.to_csv((args.folder + "/valid/1-1.txt"), sep='\t', header=None, index=None)
+test_data1_n.to_csv((args.folder + "/valid/1-n.txt"), sep='\t', header=None, index=None)
+test_datan_1.to_csv((args.folder + "/valid/n-1.txt"), sep='\t', header=None, index=None)
+test_datan_n.to_csv((args.folder + "/valid/n-n.txt"), sep='\t', header=None, index=None)
+
+test_data_grouped = test_data.groupby(1)
+
+count = 0
+for k, gr in test_data_grouped:
+    name = "relation_" + str(count)
+    gr.to_csv((args.folder + "/train/{}.txt".format(name)), sep='\t', encoding='utf-8', index=False, header=False, mode='a')
+    count += 1
